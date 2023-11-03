2f9b3ae6ca1ae3eb05d8251d0b51f8ae257ffb06
Marina Chernykh <danmarizza@gmail.com>
2023-11-03 23:30:26 +0300

    fix users styles

--- hiking/users/forms.py
+++ hiking/users/forms.py
@@ -11,4 +11,4 @@ class CreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'email')
